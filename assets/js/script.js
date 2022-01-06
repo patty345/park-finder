df242a010fc8d846015152115bb0439084482c59
Zane Scheel <zane.scheel@gmail.com>
2022-01-06 10:53:32 -0600

    started on some functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
 $(document).foundation()
+var weatherSection = $("#weatherSection");
+
+function weatherBoxes() {
+    var weatherBox = "<div> <p> Hello World </p> </div>";
+    weatherSection.append(weatherBox);
+    
+}
+
+
+weatherBoxes();
