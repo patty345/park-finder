3f7e0b59020e77f5fce7d2e4387414f2cf37cf74
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-01-08 13:46:24 -0600

    fetch call to mapquest api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-$(document).foundation()
+//$(document).foundation();
+
+
+
+  fetch("http://www.mapquestapi.com/geocoding/v1/address?key=hA7ssNDOXBS2CZMbFpA3HIjzn3G1FtIG&location=Washington,DC").then(function (response) {
+    if(response.ok) {
+        response.json().then(function(data) {
+            console.log(data)
+        })
+    }
+})
+
+
