e335b064a6fbca0b9f9991a6fac7d9d8f11736f1
zanescheel-fTEJAV <zane.scheel@gmail.com>
2022-01-10 20:38:38 -0600

    switched fetch and started cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ var cityName = document.querySelector("#city").value;
 })
 
 function weatherRequest(cityName) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&units=imperial&appid=" + apiKey;
     fetch(apiUrl)
     .then(function(response) {
     response.json().then(function(data) {
@@ -26,7 +26,6 @@ function weatherRequest(cityName) {
 
 $(document).ready(function() {
     $(".button").click(function() {
-        weatherCardCreator();
         var cityName = document.querySelector("#city").value;
         weatherRequest(cityName);
         console.log(cityName);
