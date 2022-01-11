091f303fd50813d0613c54fccf339dae74d6aa86
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-01-11 12:09:58 -0600

    fixed the alignment

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,62 +5,77 @@ var apiKey = "05f629a6a4d93216a57d8401030d50a8";
 var weatherCards = $("#weather-cards");
 const city = $("#city");
 var cityName = document.querySelector("#city").value;
-var displayParks = document.querySelector(".park-display");
+var displayParks = document.querySelector("#map");
 
-
-function getParks (lat, lon){
-    console.log(lat, lon);
-fetch("http://www.mapquestapi.com/geocoding/v1/address?key=hA7ssNDOXBS2CZMbFpA3HIjzn3G1FtIG&location=Washington,DC").then(function (response) {
-    if(response.ok) {
-        response.json().then(function(data) {
-            console.log(data)
-        })
+function getParks(lat, lon) {
+  console.log(lat, lon);
+  fetch(
+    "http://www.mapquestapi.com/geocoding/v1/address?key=hA7ssNDOXBS2CZMbFpA3HIjzn3G1FtIG&location=Washington,DC"
+  ).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
     }
-});
-fetch("https://www.mapquestapi.com/search/v4/place?location=" + lon + "%2C" + lat +"&sort=relevance&feedback=false&key=hA7ssNDOXBS2CZMbFpA3HIjzn3G1FtIG&pageSize=5&q=parks").then(function (response){
-    if(response.ok) {
-        response.json().then(function (data){
-            console.log(data);
-            for(i=0; i<data.results.length; i++) {
-                console.log("park name: " + data.results[i].name);
-                console.log("iframe src: https://www.mapquest.com/' + data.results[i].slug");
-                var pSlug = data.results[i].slug;
-                var pSlugFrame = document.createElement("iframe");
+  });
+  fetch(
+    "https://www.mapquestapi.com/search/v4/place?location=" +
+      lon +
+      "%2C" +
+      lat +
+      "&sort=relevance&feedback=false&key=hA7ssNDOXBS2CZMbFpA3HIjzn3G1FtIG&pageSize=5&q=parks"
+  ).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+        for (i = 0; i < data.results.length; i++) {
+          console.log("park name: " + data.results[i].name);
+          console.log(
+            "iframe src: https://www.mapquest.com/' + data.results[i].slug"
+          );
+          var pSlug = data.results[i].slug;
+          var pSlugFrame = document.createElement("iframe");
 
-                pSlugFrame.setAttribute("src", ('https://www.mapquest.com/' + pSlug), "scrolling", "no")
+          pSlugFrame.setAttribute(
+            "src",
+            "https://www.mapquest.com/" + pSlug,
+            "scrolling",
+            "no"
+          );
 
-                displayParks.appendChild(pSlugFrame);
-            }
-        })
+          displayParks.appendChild(pSlugFrame);
+        }
+      });
     }
-})
+  });
 }
-  
 
 function weatherRequest(cityName) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey;
-    fetch(apiUrl)
-    .then(function(response) {
-    response.json().then(function(data) {
-        weatherCardCreator(data, cityName);
-        getParks(data.city.coord.lat, data.city.coord.lon);
-    })
-})
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&appid=" +
+    apiKey;
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      weatherCardCreator(data, cityName);
+      getParks(data.city.coord.lat, data.city.coord.lon);
+    });
+  });
 }
 
-    
-$(document).ready(function() {
-    $(".button").click(function() {
-        weatherCardCreator();
-        var cityName = document.querySelector("#city").value;
-        weatherRequest(cityName);
-        console.log(cityName);
-        var history = $("#history-list")
-        $(history).append('<li><p>' + cityName + '</p></li>');
+$(document).ready(function () {
+  $(".button").click(function () {
+    weatherCardCreator();
+    var cityName = document.querySelector("#city").value;
+    weatherRequest(cityName);
+    console.log(cityName);
+    var history = $("#history-list");
+    $(history).append("<li><p>" + cityName + "</p></li>");
 
-        localStorage.setItem("city", city);
-    })
-})
+    localStorage.setItem("city", city);
+  });
+});
 
 // var map = document.querySelector(".map");
 
@@ -76,18 +91,14 @@ $(document).ready(function() {
 //     map.addControl(L.mapquest.control());
 //   }
 
-
-
-
 function weatherCardCreator(data, cityName) {
-    var container = $("<div class='weather-card cell margin-1 small-12 medium-6'></div>");
-    var day = $("<p>1/1/11</p>");
-    console.log(data);
-    var rainChance = $("<p>1.1%</p>");
-    container.append(day);
-    container.append(rainChance);
-    weatherCards.append(container);
+  var container = $(
+    "<div class='weather-card cell margin-1 small-12 medium-6'></div>"
+  );
+  var day = $("<p>1/1/11</p>");
+  console.log(data);
+  var rainChance = $("<p>1.1%</p>");
+  container.append(day);
+  container.append(rainChance);
+  weatherCards.append(container);
 }
-
-
-
